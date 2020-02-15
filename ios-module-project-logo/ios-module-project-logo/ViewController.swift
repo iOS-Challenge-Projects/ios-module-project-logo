9094abd0ad3e78dd10a5613f62b72f41a2280cef
fritzgt <tuanis@live.com>
2020-02-15 16:59:04 +0200

    initial commit

---
+++ ios-module-project-logo/ios-module-project-logo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-module-project-logo
+//
+//  Created by FGT MAC on 2/15/20.
+//  Copyright © 2020 FGT MAC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var customLogoDraw: LogoView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    
+      
+    }
+
+
+}
+
