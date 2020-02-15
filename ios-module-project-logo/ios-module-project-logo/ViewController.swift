3ef614a0c80d400a9713991a940630a6ec90a4fa
fritzgt <tuanis@live.com>
2020-02-15 18:12:29 +0200

    completed multi line

--- ios-module-project-logo/ios-module-project-logo/ViewController.swift
+++ ios-module-project-logo/ios-module-project-logo/ViewController.swift
@@ -26,31 +26,57 @@ class Canvas: UIView {
 //        context.move(to: startPoint)
 //        context.addLine(to: endPoint)
         
+        //Change the color
+        context.setStrokeColor(UIColor.red.cgColor)
+        
+        //Adjust the tickness
+        context.setLineWidth(10)
+        
         //Draw the lines by iterating on the array with the index
-        for (i, p) in line.enumerated(){
-            if i == 0{
-                context.move(to: p)
-            }else{
-                context.addLine(to: p)
+        //Nested loop to draw each of the lines inside the lines array which is a 2d array
+        lines.forEach { (line) in
+            
+            for (i, p) in line.enumerated(){
+                if i == 0{
+                    context.move(to: p)
+                }else{
+                    context.addLine(to: p)
+                }
             }
         }
         
         
-        
         context.strokePath()
-
+        
     }
     
     //track line by initializing an empty array of CGPoints
-    var line = [CGPoint]()
+    //    var line = [CGPoint]()
+    
+    //this is so that we can draw multiple lines other while it will be a continues line
+    var lines = [[CGPoint]]()
+    
+    
+    //THIS will append multiple lines
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        lines.append([CGPoint]())
+    }
+    
     
     //Track finger as we move across the screen
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         
         guard let point = touches.first?.location(in: nil) else {return}
         
+        //Removes the last line and append it
+        guard var lastLine = lines.popLast() else { return }
+        lastLine.append(point)
+        
+        //added back
+        lines.append(lastLine)
+
 //        print(point)
-        line.append(point)
+//        line.append(point)
         
         //Redraw to show lines while touching
         setNeedsDisplay()
